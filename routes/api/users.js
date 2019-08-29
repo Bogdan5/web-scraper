002747343bd53ad5dd4f2576153db61f1254d4ae
Bogdan5 <bogdan5x@gmail.com>
2019-08-28 23:10:57 -0400

    Styling cleanup

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,15 @@
-const express = require("express");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+
 require('custom-env').env();
 
-const validateLoginInput = require("../../validation/login");
+const validateLoginInput = require('../../validation/login');
+const db = require('../../db');
 
+
+router.post('/', (req, res, next) => {
+  db.query
+});
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const pg = require('pg');
 
 const app = express();
+const users = require('./routes/api/users');
 
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
-  })
+    extended: false,
+  }),
 );
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.use('/register', users);
+
+// process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
