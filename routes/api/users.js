110b899953d6492945e02f2ac71c1d72a378c874
Bogdan5 <bogdan5x@gmail.com>
2019-08-29 23:09:15 -0400

    Adding routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,10 +6,19 @@ const router = express.Router();
 
 require('custom-env').env();
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require('../../validation/login');
 const db = require('../../db');
 
 
-router.post('/', (req, res, next) => {
-  db.query
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  db.query('SELECT email FROM users WHERE ')
+  db.query()
 });
