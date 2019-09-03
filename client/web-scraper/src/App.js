66e46ef02e18c8784fa6c4962beb268bb1d2a2bb
Bogdan5 <bogdan5x@gmail.com>
2019-09-02 22:45:37 -0400

    Basic input in registration

--- client/web-scraper/src/App.js
+++ client/web-scraper/src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { threadId } from 'worker_threads';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(){
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      password2: ''
+    }
+  }
 
-    </div>
-  );
+  handler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  buttonClick = (e) => {
+    const { username, email, password, password2 } = this.state;
+    e.preventDefault();
+    const user = {
+      username,
+      email,
+      password,
+      password2
+
+    };
+    axios.post('/register', user)
+      .then((res) => {
+        if (res.data.success) {
+          
+        } else {
+          console.log('if false');
+          var error = new Error('Error ' + res.status + ': ' + res.statusText);
+          error.res = res;
+          throw error;
+        }
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+        // this.setState({ errors: err.response.data });
+      });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form>
+          <input type='text' onChange={this.inputHandler} name='username' />
+          <input type='text' onChange={this.inputHandler} name='email' />
+          <input type='text' onChange={this.inputHandler} name='password' />
+          <input type='text' onChange={this.inputHandler} name='password2' />
+          <button type='button' onClick={this.buttonClick} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
