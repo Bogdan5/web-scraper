988904886e4f204d1fbaed2477b71a60c9e5f9d4
Bogdan5 <bogdan5x@gmail.com>
2019-09-04 23:16:45 -0400

    Authentication signin

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,7 +16,7 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
 
-router.post('/', (req, res) => {
+router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
   // Check validation
   if (!isValid) {
@@ -57,18 +57,72 @@ router.post('/', (req, res) => {
             });
           }
         });
-
-      // client.query('INSERT INTO users(username, email, password) VALUES($1 $2 $3)',
-      //   [req.body.username, req.body.email, req.body.password], (error, results) => {
-      //     if (error) {
-      //       console.error('Error in connection ', error);
-      //       throw error;
-      //     } else {
-      //       res.redirect('/');
-      //     }
-      //   });
     }
   });
+});
+
+router.post('/login', (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const { email, password } = req.body;
+  // Find user by email
+  pool.connect((err, client, done) => {
+    const { username, email, password } = req.body;
+    if (err) {
+      console.error('Database connection failed', err);
+    } else {
+      // check if email already used
+
+      console.log('Done', email);
+      client.query('SELECT email FROM users WHERE email=$1', [email],
+        (error, result) => {
+          if (error) {
+            console.error('Error in connection selecting ', error);
+            return res.status(400).send(err);
+          }
+
+          console.log('Response is ', result.rows);
+          if (!result.rows.length) {
+            return res.status(404).json({ emailnotfound: 'Email not found' });
+          }
+        });
+    }
+  });
+
+  // Check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User matched
+          // Create JWT Payload
+          const payload = {
+            id: user.id,
+            name: user.name
+          };
+  // Sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {
+              expiresIn: 31556926 // 1 year in seconds
+            },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            }
+          );
+        } else {
+          return res
+            .status(400)
+            .json({ passwordincorrect: 'Password incorrect' });
+        }
+      });
+    });
 });
 
 module.exports = router;
--- validation/login.js
+++ validation/login.js
@@ -20,6 +20,6 @@ module.exports = function validateLoginInput(data) {
   }
   return {
     errors,
-    isValid: isEmpty(errors)
+    isValid: isEmpty(errors),
   };
 };
