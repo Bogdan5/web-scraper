22db0b3f07beedbfacbb7e5a370a5c8e57d858b8
Bogdan5 <bogdan5x@gmail.com>
2019-09-08 21:54:55 -0400

    Redux boilerplate contd

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import { Nav } from 'react-bootstrap';
 import Register from './components/Register.js';
 import Login from './components/Login.js';
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 
 class App extends Component {
   constructor(props){
@@ -19,29 +22,31 @@ class App extends Component {
   render(){
     const { registered } = this.state;
     return (
-      <div className='App'>
-        <Nav variant="pills">
-          <Nav.Item>
-            <Nav.Link as={Link} eventKey="1" to='/' exact >
-              Home
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} eventKey="2" active={registered} to='/login'>
-              Login
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} eventKey="3" to='/register'>
-              Register
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Switch>
-          <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <div className='App'>
+          <Nav variant="pills">
+            <Nav.Item>
+              <Nav.Link as={Link} eventKey="1" to='/' exact >
+                Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} eventKey="2" active={registered} to='/login'>
+                Login
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} eventKey="3" to='/register'>
+                Register
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Switch>
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+          </Switch>
+        </div>
+      </Provider>
     );
   }
 }
--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
